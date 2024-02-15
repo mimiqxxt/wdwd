e5d89d6f26917d5f19a6840f08070e0ffbc1e418
mimiqxxt <160110428+mimiqxxt@users.noreply.github.com>
2024-02-15 16:03:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["being so cute!<3","what.."];
 
 
 let currentIndex = 0;
